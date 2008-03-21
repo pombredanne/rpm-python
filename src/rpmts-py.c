a45c4745ae81445b02c4d963205df918f79533af
Panu Matilainen <pmatilai@laiskiainen.org>
2008-03-21 13:15:42 +0200

    Typo...

--- src/rpmts-py.c
+++ src/rpmts-py.c
@@ -1358,7 +1358,7 @@ PyTypeObject rpmts_Type = {
 	0,				/* tp_hash */
 	0,				/* tp_call */
 	0,				/* tp_str */
-	PyObject_GenericGetAttr,, 	/* tp_getattro */
+	PyObject_GenericGetAttr, 	/* tp_getattro */
 	(setattrofunc) rpmts_setattro,	/* tp_setattro */
 	0,				/* tp_as_buffer */
 	Py_TPFLAGS_DEFAULT, 		/* tp_flags */
