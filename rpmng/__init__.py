538e4d45c3eb800cc5ea2ffed04d7aef50ca4e96
Panu Matilainen <pmatilai@redhat.com>
2008-04-08 10:42:33 +0300

    Add _RPMVSF_* items manually for now..

--- rpmng/__init__.py
+++ rpmng/__init__.py
@@ -7,4 +7,8 @@ This module enables you to manipulate rpms and the rpm database.
 from _rpmng import *
 
 import _rpmng 
+_RPMVSF_NODIGESTS = _rpmng._RPMVSF_NODIGESTS
+_RPMVSF_NOHEADER = _rpmng._RPMVSF_NOHEADER
+_RPMVSF_NOPAYLOAD = _rpmng._RPMVSF_NOPAYLOAD
+_RPMVSF_NOSIGNATURES = _rpmng._RPMVSF_NOSIGNATURES
 
