4c44123a61725d5a7dfba275e3ca8b5222885d7b
Panu Matilainen <pmatilai@redhat.com>
2008-05-12 16:25:30 +0300

    Use headerFormat() instead of headerSprintf()

--- src/header-py.c
+++ src/header-py.c
@@ -217,7 +217,7 @@ static PyObject * hdrSprintf(hdrObject * s, PyObject * args, PyObject * kwds)
     if (!PyArg_ParseTupleAndKeywords(args, kwds, "s", kwlist, &fmt))
 	return NULL;
 
-    r = headerSprintf(s->h, fmt, rpmTagTable, rpmHeaderFormats, &err);
+    r = headerFormat(s->h, fmt, &err);
     if (!r) {
 	PyErr_SetString(pyrpmError, err);
 	return NULL;
@@ -310,7 +310,7 @@ static int dressedHeaderGetEntry(Header h, rpmTag tag, rpmTagType *type,
 	fmt = rstrscat(NULL, "%{", rpmTagGetName(tag), "}\n", NULL);
 	
 	/* XXX FIXME: memory leak. */
-        msgstr = headerSprintf(h, fmt, rpmTagTable, rpmHeaderFormats, &errstr);
+        msgstr = headerFormat(h, fmt, &errstr);
 	free(fmt);
 	if (msgstr) {
 	    *p = (void *) msgstr;
